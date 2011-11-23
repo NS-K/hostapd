70b4b2473860f96e782bf154d562fe8bcc8c56c8
Jouni Malinen <jouni@qca.qualcomm.com>
2011-11-23 17:48:31 +0200

    Android: Convert driver_cmd BGSCAN-START/STOP to proper driver_ops
    Instead of driver specific specific command, use a cleaner design
    for handling the Android BGSCAN-START/STOP commands.

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -5164,7 +5164,12 @@ static int wpa_supplicant_driver_cmd(struct wpa_supplicant *wpa_s, char *cmd,
 {
 	int ret;
 
-	ret = wpa_drv_driver_cmd(wpa_s, cmd, buf, buflen);
+	if (os_strcasecmp(cmd, "BGSCAN-START") == 0)
+		ret = pno_start(wpa_s);
+	else if (os_strcasecmp(cmd, "BGSCAN-STOP") == 0)
+		ret = pno_stop(wpa_s);
+	else
+		ret = wpa_drv_driver_cmd(wpa_s, cmd, buf, buflen);
 	if (ret == 0) {
 		if (os_strncasecmp(cmd, "COUNTRY", 7) == 0) {
 			struct p2p_data *p2p = wpa_s->global->p2p;
